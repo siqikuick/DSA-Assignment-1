8eedc95b4af3b66adeeb960c6309189db91a08af
chocolate-neko <44422224+chocolate-neko@users.noreply.github.com>
2024-02-02 17:46:00 +0800

    Update test cases

--- src/main.c
+++ src/main.c
@@ -6,26 +6,47 @@ int main(int argc, char const* argv[])
 {
     List* list = adt_new();
 
-    // Append elements to the list
+    printf("=== Original list ===\n");
+
     adt_append(list, 10);
     adt_append(list, 20);
     adt_append(list, 30);
 
-    // Insert an element after the second node
+    adt_print(list);
+
+    printf("### Tests ###\n\n");
+
     ListNode* node = adt_get(list, 1, 20);
     adt_insert_after(list, node, 25);
 
-    // Print the list
-    adt_print(list); // Expected output: 30 25 20 10
+    printf("=== List after inserting 25 after 20 ===\n");
+    adt_print(list);
 
-    // Remove the second node
     adt_remove(list, 1);
 
-    // Print the list again
-    adt_print(list); // Expected output: 30 20 10
+    printf("=== List after removing element 1 (25) ===\n");
+    adt_print(list);
 
-    // Destroy the list
+    adt_remove(list, 0);
+    adt_remove(list, 0);
+
+    printf("=== List after removing element 2x 0 ===\n");
+    adt_print(list);
+
     adt_destroy(list);
+    printf("Test complete...\n");
+
+    printf("### Edge case tests ###\n\n");
+
+    List* edge_list = adt_new();
+
+    printf("=== Empty list print ===\n");
+    adt_print(edge_list);
+
+    printf("=== List after removing element from empty list ===\n");
+    adt_remove(edge_list, 0);
+
+    adt_destroy(edge_list);
 
     return 0;
 }
